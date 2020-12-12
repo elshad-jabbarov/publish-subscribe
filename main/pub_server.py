1af307b5b48a0d051b745f019695d5bc5c3eba6c
elshad-jabbarov <jabbarov.elshad@gmail.com>
2020-12-12 21:59:47 +0400

    sending list items

--- main/pub_server.py
+++ main/pub_server.py
@@ -5,9 +5,12 @@ import zmq
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind('tcp://127.0.0.1:2000')
-my_list=folder.contents(input("Please enter folder path: "))
-
+my_list = folder.contents(input("Please enter folder path: "))
+k = len(my_list) - 1
 
 while True:
-    time.sleep(1)
-    socket.send_string("FOLDER" + '\t%s' % my_list.pop())
+    time.sleep(2)
+    socket.send_string("FOLDER" + '\t%s' % my_list.__getitem__(k))
+    k -= 1
+    if k == -1:
+        k = len(my_list) - 1
--- main/sub_client.py
+++ main/sub_client.py
@@ -6,5 +6,5 @@ socket.connect('tcp://127.0.0.1:2000')
 socket.setsockopt_string(zmq.SUBSCRIBE, 'FOLDER')
 
 while True:
-    folder=socket.recv_string()
+    folder = socket.recv_string()
     print(folder)
