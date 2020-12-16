5691cabe8d7e245c20963f8fac2172a021ffdf30
elshad-jabbarov <jabbarov.elshad@gmail.com>
2020-12-16 09:25:38 +0400

    sending list items

--- main/folder.py
+++ main/folder.py
@@ -5,6 +5,6 @@ def contents(root):
     list_of_contents = []
     for item in os.listdir(root):
         if os.path.isdir or os.path.isfile(os.path.join(root, item)):
-            k = (root + "\ " + item)
+            k = (root + "\ has " + item)
             list_of_contents.append(k)
     return list_of_contents
--- main/pub_server.py
+++ main/pub_server.py
@@ -7,10 +7,11 @@ socket = context.socket(zmq.PUB)
 socket.bind('tcp://127.0.0.1:2000')
 my_list = folder.contents(input("Please enter folder path: "))
 k = len(my_list) - 1
-
+print("Publisher started")
 while True:
-    time.sleep(2)
+    time.sleep(1)
     socket.send_string("FOLDER" + '\t%s' % my_list.__getitem__(k))
     k -= 1
     if k == -1:
+        socket.send_string("FOLDER" + '\t%s' % "--------finished---------")
         k = len(my_list) - 1
